39151d5eeb56260514ddf9f0354ed55af99c89a8
Kirill Karasev <karasev.kirill.a@gmail.com>
2019-05-22 23:15:59 +1000

    Удаление конструктора из лота

--- ParserLibrary/Classes/Lot.cs
+++ ParserLibrary/Classes/Lot.cs
@@ -33,25 +33,6 @@
         public bool UseTrashwords { get; set; }
 
         /// <summary>
-        /// Инициализация полей лота
-        /// </summary>
-        /// <param name="price">Цена</param>
-        /// <param name="title">Заголовок</param>
-        /// <param name="link">Ссылка на лот</param>
-        /// <param name="city">Город продажи лота</param>
-        /// <param name="trashwords">Лишние слова, которые необходимо удалить из заголовка</param>
-        /// <param name="useTrashwords">Признак удаления лишних слов</param>
-        //public Lot(string price, string title, string link, string city, string[] trashwords, bool useTrashwords)
-        //{
-        //    Price = RemoveNewline(price);
-        //    Title = RemoveNewline(title);
-        //    Link = RemoveNewline(link);
-        //    City = RemoveNewline(city);
-        //    Trashwords = trashwords;
-        //    UseTrashwords = useTrashwords;
-        //}
-
-        /// <summary>
         /// Возвращает данные лота в пригодном для записи в .csv файл виде
         /// </summary>
         /// <returns></returns>
